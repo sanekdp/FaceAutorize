cb22930a5cd1d5b19b3b6d0162f090eed27df808
sanekdp <sanekdp@gmail.com>
2017-02-01 20:52:12 +0200

    start2

--- app/src/main/java/com/levelup/facebookautorize/MainActivity.java
+++ app/src/main/java/com/levelup/facebookautorize/MainActivity.java
@@ -3,25 +3,52 @@ package com.levelup.facebookautorize;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 0;
     CallbackManager callbackManager;
+    GoogleApiClient mGoogleApiClient = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, fail -> {
+
+                } /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
         callbackManager = CallbackManager.Factory.create();
         Button mButtonFaceBook = (Button) findViewById(R.id.bt_facebook);
+        Button mButtonGoogle = (Button) findViewById(R.id.bt_googleplus);
+
+        mButtonGoogle.setOnClickListener(click -> {
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        });
 
         mButtonFaceBook.setOnClickListener(click -> {
             LoginManager.getInstance().logInWithReadPermissions(
@@ -48,9 +75,29 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("TAG", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            //mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            //updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            //updateUI(false);
+        }
     }
 }
--- app/src/main/java/com/levelup/facebookautorize/MyClass.java
+++ app/src/main/java/com/levelup/facebookautorize/MyClass.java
@@ -3,13 +3,18 @@ package com.levelup.facebookautorize;
 import android.app.Application;
 
 import com.facebook.appevents.AppEventsLogger;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
 public class MyClass extends Application {
+
+
     @Override
     public void onCreate() {
         super.onCreate();
         //FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
+
     }
 }
